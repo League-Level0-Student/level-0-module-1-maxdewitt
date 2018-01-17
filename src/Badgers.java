7817c52a2e71ceb73912c8cc9f2342d4aab307e3
Dewitt.Max <Dewitt.Max@SEMS-259-09.SEMS259.LAB>
2018-01-17 14:38:05 -0800

    badgers

--- src/Badgers.java
+++ src/Badgers.java
@@ -1,14 +1,37 @@
 
 public class Badgers {
-	Badger, badger, badger, badger, badger, badger, badger, badger, badger, badger
-	Mushroom, mushroom, a
-	Badger, badger, badger, badger, badger, badger, badger, badger, badger, badger
-	Mushroom, mushroom, a
-	Badger, badger, badger, badger, badger, badger, badger, badger, badger, badger
-	Mush-mushroom, a
-	Badger, badger, badger, badger, badger, badger, badger, badger, badger, badger
-	African! Snake, a snake
-	Snake! A snake, ooh it's a snake
-	It's a badger, badger, badger, badger, badger, badger, badger, badger, badger, badger
-	Mushroom, mushroom,
+	public static void main(String[] args) {
+
+		/*
+		 * Badger, badger, badger, badger, badger, badger, badger, badger, badger,
+		 * badger Mushroom, mushroom, Badger, badger, badger, badger, badger, badger,
+		 * badger, badger, badger, badger Mushroom, mushroom, Badger, badger, badger,
+		 * badger, badger, badger, badger, badger, badger, badger Mush-mushroom, Badger,
+		 * badger, badger, badger, badger, badger, badger, badger, badger, badger Snake,
+		 * a snake Snake! A snake, ooh it's a snake badger, badger, badger, badger,
+		 * badger, badger, badger, badger, badger, badger Mushroom, mushroom,
+		 */
+		for (int q = 0; q < 3; q++) {
+
+			for (int i = 0; i < 10; i++) {
+				System.out.print("badger, ");
+			}
+			System.out.println();
+			for (int o = 0; o < 2; o++) {
+
+				System.out.print("mushroom, ");
+
+			}
+			System.out.println();
+			if (q == 2) {
+				for (int s = 0; s < 4; s++) {
+					System.out.print("snake! ");
+				    if (s == 2) {
+				    System.out.print("oh its a ");
+				    }
+			}
+		}
+	}
+
+}
 }
